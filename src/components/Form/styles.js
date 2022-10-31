dfc01f0e80103fc69e65489bbe47544c851fffc0
andrzul <aineken93@gmail.com>
2022-10-31 21:50:08 +0100

    stories project commit to github

---
+++ src/components/Form/styles.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export default createTheme({
+  form: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: "10px",
+    padding: "10px",
+  },
+  fileInput: {
+    width: "100%",
+  },
+  buttonSubmit: {
+    // marginBottom: "10px",
+  },
+});
